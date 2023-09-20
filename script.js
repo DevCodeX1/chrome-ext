e8eb0f6627826da54f3c4d00ce29eff6d8b9fbde
DevCodeX <countryboya20@gmail.com>
2023-09-19 21:32:10 -0500

    finail

--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-
-let myLeeds = []
+let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const delBtn = document.getElementById("delete-el")
+const leedsFromLocalStorage = JSON.parse(localStorage.getItem("myLeeds"))
+const tabBtn = document.getElementById("tab-btn")
 
-
-let leedsFromLocalStorage = JSON.parse(localStorage.getItem("myLeeds"))
 
 if(leedsFromLocalStorage){
-    myLeeds = leedsFromLocalStorage
-    renderLeads()
+    myLeads = leedsFromLocalStorage
+    render(myLeads)
+}
+
+// Saves input to a neseted list on the screen
+function render(leads){
+    let listItems = "";
+    for(let i = 0; i < leads.length; i++){
+        listItems += `
+        <li>
+            <a href = "${leads[i]}" target="_blank">
+                ${leads[i]}
+            </a>
+        </li>` 
+        // Second way to create an element 
+        // create element
+        // set text 
+        // append to ul
+        // const li = document.createElement("li")
+        // li.textContent = myLeeds[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+
 }
 
 // Added save button
 inputBtn.addEventListener('click', function(){
-    myLeeds.push(inputEl.value)
+    myLeads.push(inputEl.value)
     inputEl.value = "";
-    localStorage.setItem("myLeeds", JSON.stringify(myLeeds))
-    renderLeads()
-
-  console.log(localStorage.getItem("myLeeds"))
+    localStorage.setItem("myLeeds", JSON.stringify(myLeads))
+    render(myLeads)
 })
-
 
 
 // Added delete button 
 delBtn.addEventListener('click', function(){
-    myLeeds.pop(inputEl.value)
-    localStorage.clear("myLeeds", JSON.stringify(myLeeds))
-    renderLeads()
-
-  
+   localStorage.clear()
+   myLeads = []
+   render(myLeads)
+    
 })
 
-// Saves input to a neseted list on the screen
-function renderLeads(){
-let listItems = "";
-for(let i = 0; i < myLeeds.length; i++){
-    listItems += `
-    <li>
-        <a href = "${myLeeds[i]}" target="_blank">
-            ${myLeeds[i]}
-        </a>
-    </li>` 
-    // Second way to create an element 
-    // create element
-    // set text 
-    // append to ul
-    // const li = document.createElement("li")
-    // li.textContent = myLeeds[i]
-    // ulEl.append(li)
-}
-ulEl.innerHTML = listItems
-}
+// Added save tab button
+tabBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeeds", JSON.stringify(myLeads))
+        render(myLeads)
+
+    })
+       
+})
+
+
